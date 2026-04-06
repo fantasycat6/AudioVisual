abbee19cedd1c6db1aa0d357124ae328a5a55130
fantasycat6 <1745272762@qq.com>
2026-04-07 02:14:43 +0800

    AudioVisual_Web nodejs

---
+++ GitUpload.cmd
@@ -0,0 +1,8 @@
+@echo off
+::git init
+git add .
+git commit -m "AudioVisual_Web nodejs"
+::git remote add origin https://github.com/fantasycat6/AudioVisual.git
+::git checkout -b nodejs
+git push -u origin nodejs
+cmd
